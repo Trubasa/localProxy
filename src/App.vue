3141a3918f7e7f8349acc5eefdd3420a0323fa84
trubasa <1141521502@qq.com>
2018-10-23 09:39:46 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
